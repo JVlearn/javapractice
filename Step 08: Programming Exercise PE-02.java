4dfc7099ee62fdbc0096c487639b354de190f416
JVlearn <142486720+JVlearn@users.noreply.github.com>
2024-06-09 16:03:04 +0800

    Create Step 08: Programming Exercise PE-02.java

---
+++ Step 08: Programming Exercise PE-02.java
@@ -0,0 +1,14 @@
+/*Try and solve the following exercises:
+
+1. Print Hello World onto the console.
+  System.out.println("Hello World")
+  
+2. Print 5 * 3 , as is.
+  System.out.println("5 * 3 ")
+  
+3. Print the calculated value of 5 * 3 .
+  System.out.println(5 * 3)
+  
+4. Print the number of seconds in a day, using the System.out.println method.
+  System.out.println(60*60*24)
+  */
