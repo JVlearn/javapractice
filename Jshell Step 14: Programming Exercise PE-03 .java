f8f255faecdb6db07285bf00c6b6d9d738a96e7b
JVlearn <142486720+JVlearn@users.noreply.github.com>
2024-06-12 00:51:38 +0800

    Create Jshell Step 14: Programming Exercise PE-03 .java

---
+++ Jshell Step 14: Programming Exercise PE-03 .java
@@ -0,0 +1,33 @@
+/*
+
+Step 14: Programming Exercise PE-03
+
+Create three integer variables a , b and c .
+
+jshell> int a=2,b=3,c=4
+a ==> 2
+b ==> 3
+c ==> 4
+
+Write a statement to print the sum of these three variables.
+
+jshell> System.out.printf("%d + %d + %d = %d", a,b,c,a+b+c).println()
+2 + 3 + 4 = 9
+
+Change the value of a , and then print this sum.
+
+jshell> a=11
+a ==> 11
+
+jshell> System.out.printf("%d + %d + %d = %d", a,b,c,a+b+c).println()
+11 + 3 + 4 = 18
+
+Then again, change the value of b and print this sum.
+
+jshell> b=100
+b ==> 100
+
+jshell> System.out.printf("%d + %d + %d = %d", a,b,c,a+b+c).println()
+11 + 100 + 4 = 115
+
+*/
