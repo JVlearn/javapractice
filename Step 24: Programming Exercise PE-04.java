66389636cb5ffa3d857b3f0255b67532180b4288
JVlearn <142486720+JVlearn@users.noreply.github.com>
2024-06-13 21:34:53 +0800

    Create Step 24: Programming Exercise PE-04.java

---
+++ Step 24: Programming Exercise PE-04.java
@@ -0,0 +1,41 @@
+/*
+
+
+1. Create four integer variables a , b , c and d . Write an if statement to print if the sum of a and b is
+greater than the sum of c and d .
+
+jshell> if ( a +b < c+d)
+   ...> System.out.println("Sum of a and b is greater than c and d");
+Sum of a and b is greater than c and d
+
+2. Store three numerical values as proposed angles of a triangle in integer variables angle1 , angle2 and
+angle3 . Create an if statement to state whether these three angles together can form a triangle.
+
+Hint: A triangle is a closed geometric figure with three angles, whose sum must exactly equal 180 degrees .
+
+jshell> int angleOne = 55
+angleOne ==> 55
+jshell> int angleTwo = 65
+angleOne ==> 55
+jshell> int angleThree = 60
+angleOne ==> 55
+jshell> if (angleOne + angleTwo + angleThree == 180)
+...> System.out.println("The three angles together form a triangle");
+The three angles together form a triangle
+jshell> angleThree = 75
+angleOne ==> 55
+jshell> if (angleOne + angleTwo + angleThree == 180)
+...> System.out.println("The three angles together form a triangle");
+
+
+3. Have a variable store an integer. Create an if statement to find out if it's an even number.
+Hint: Use operator % .
+
+jshell> int number1 = 10;
+number1 ==> 10
+
+jshell> if ( number1 % 2 == 0)
+   ...> System.out.println("The stored value is even");
+The stored value is even
+
+*/
