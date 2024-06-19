c5ca2c2c33e03a648316546234aa4048a757d1c5
JVlearn <142486720+JVlearn@users.noreply.github.com>
2024-06-19 15:17:05 +0800

    Create Jshell Methods Step 02: Exercise-Set PE-01.java

---
+++ Jshell Methods Step 02: Exercise-Set PE-01.java
@@ -0,0 +1,38 @@
+/*
+
+1.Write and execute a method named sayHelloWorldThrice to print Hello World thrice.
+
+jshell> void sayHelloWorldThrice(){
+   ...> System.out.println("Hello, World");
+   ...> System.out.println("Hello, World");
+   ...> System.out.println("Hello, World");
+   ...> }
+|  created method sayHelloWorldThrice()
+
+jshell> sayHelloWorldThrice()
+Hello, World
+Hello, World
+Hello, World
+
+2. Write and execute a method that prints the following four statements:
+
+I've created my first variable
+I've created my first loop
+I've created my first method
+I'm excited to learn Java
+
+jshell> void printFourStatements(){
+   ...> System.out.println("I've Created my first variable");
+   ...> System.out.println("I've Created my first loop");
+   ...> System.out.println("I've created my first method");
+   ...> System.out.println("I'm excitdd to learn JAVA");
+   ...> }
+|  created method printFourStatements()
+
+jshell> printFourStatements()
+I've Created my first variable
+I've Created my first loop
+I've created my first method
+I'm excitdd to learn JAVA
+
+*/
